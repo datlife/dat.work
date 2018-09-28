08855aef09930025d4c5e8a7a3b3efe284db8b81
Dat <tdat.nguyen93@gmail.com>
2018-09-28 04:22:13 -0700

    Added Footer

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import "./style.scss";
+
+class Footer extends Component {
+  render(){
+    return(
+      <div className="footer">
+        <p>"Where is a will, there is a way"</p>
+      </div>
+    )
+  }
+}
+
+export default Footer;
