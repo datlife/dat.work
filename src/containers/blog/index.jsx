241e599d64105f480e7c51bbef73d5355374f18a
Dat Nguyen <tdat.nguyen93@gmail.com>
2018-10-03 00:11:38 -0700

    Working API

--- src/containers/blog/index.jsx
+++ src/containers/blog/index.jsx
@@ -2,16 +2,20 @@ import React, { Component } from 'react'
 import './style.scss'
 
 class Blog extends Component {
+  async getPosts(){
+    const res = await fetch("http://35.208.9.206/ghost/api/v0.1/posts/?include=tags&limits=3&formats=json&client_id=ghost-frontend&client_secret=ec2e9f4fefe0")
+    const posts = await res.json();
+    return posts;
+  }
   render() {
-    fetch("http://35.208.9.206/ghost/api/v0.1/posts/?include=tags&limits=3&formats=plaintext&client_id=ghost-frontend&client_secret=ec2e9f4fefe0")
-      .then(res => {console.log(res.json())})
-      .then( data => {console.log(data) })
+    console.log(this.getPosts())
     return (
       <div className="blog">
         <h3>This is a Blog</h3>
       </div>
     )
   }
+
 }
 
 export default Blog;
